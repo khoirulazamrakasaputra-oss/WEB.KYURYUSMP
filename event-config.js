75fb559492eaf3fd6bb06649ec571bbe5c5d4218
RAKSJA <khoirulazamrakasaputra@gmail.com>
2026-09-15 22:28:42 +0700

    Update event-config.js

--- event-config.js
+++ event-config.js
@@ -11,7 +11,7 @@
 
 const EVENT_CONFIG = [
   {
-    aktif: true,
+    aktif: false,
     nama: "ewe azel",
     tanggal: "15 September 2026",
     waktu: "19:00 WIB",
@@ -20,7 +20,7 @@ const EVENT_CONFIG = [
     gambar: "https://images.unsplash.com/photo-1544551763-46a013bb70d5?w=600&auto=format&fit=crop&q=80"
   },
   {
-    aktif: true,
+    aktif: false,
     nama: "Mining Competition",
     tanggal: "22 September 2026",
     waktu: "20:00 WIB",
